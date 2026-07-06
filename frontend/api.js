989df759a0b9171063881f7faa603ac5b97836fa
aribaprvt <aribamalickprvt@gmail.com>
2026-07-06 15:12:50 +0500

    Update api.js

--- frontend/api.js
+++ frontend/api.js
@@ -1,15 +1,7 @@
 const API_URL = "http://127.0.0.1:8000";
-
-// Helper to get the saved authorization token
 function getAuthToken() {
   return localStorage.getItem("access_token");
 }
- 
-
-
-
-
-// Helper to log out
 function logout() {
   localStorage.removeItem("access_token");
   window.location.href = "login.html";
